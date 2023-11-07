fe640d9a67308f6641492f7de8989bb4035d0966
kxelina <elina88.k@gmail.com>
2023-11-07 19:05:20 +0200

    pylint

--- src/index.py
+++ src/index.py
@@ -4,54 +4,50 @@ from varasto import Varasto
 def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
+    print(f'''Luonnin jälkeen:"
+Mehuvarasto: {mehua}" 
+Olutvarasto: {olutta}
+Olut getterit:
+saldo = {olutta.saldo}"
+tilavuus = {olutta.tilavuus}"
+paljonko_mahtuu = {olutta.paljonko_mahtuu()}
+Mehu setterit: Lisätään 50.7''')
+    huonoa(mehua, olutta)
 
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
-
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
-
-    print("Mehu setterit:")
-    print("Lisätään 50.7")
-    mehua.lisaa_varastoon(50.7)
-    print(f"Mehuvarasto: {mehua}")
-    print("Otetaan 3.14")
-    mehua.ota_varastosta(3.14)
-    print(f"Mehuvarasto: {mehua}")
-
-    print("Virhetilanteita:")
-    print("Varasto(-100.0);")
+def huonoa(mehua, olutta):
     huono = Varasto(-100.0)
-    print(huono)
-
-    print("Varasto(100.0, -50.7)")
+    mehua.lisaa_varastoon(50.7)
+    print(f'''Mehuvarasto: {mehua}
+Otetaan 3.14''')
+    mehua.ota_varastosta(3.14)
+    print(f'''Mehuvarasto: {mehua}
+Virhetilanteita:
+Varasto(-100.0);
+{huono}
+Varasto(100.0, -50.7)''')
     huono = Varasto(100.0, -50.7)
-    print(huono)
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.lisaa_varastoon(1000.0)")
+    print(f'''{huono}
+Olutvarasto: {olutta}"
+olutta.lisaa_varastoon(1000.0)''')
     olutta.lisaa_varastoon(1000.0)
-    print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.lisaa_varastoon(-666.0)")
+    print(f'''Olutvarasto: {olutta}"
+Mehuvarasto: {mehua}")
+mehua.lisaa_varastoon(-666.0)''')
     mehua.lisaa_varastoon(-666.0)
-    print(f"Mehuvarasto: {mehua}")
+    mehuaa(mehua, olutta)
 
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.ota_varastosta(1000.0)")
+def mehuaa(mehua, olutta):
     saatiin = olutta.ota_varastosta(1000.0)
-    print(f"saatiin {saatiin}")
-    print(f"Olutvarasto: {olutta}")
-
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.otaVarastosta(-32.9)")
+    print(f'''Mehuvarasto: {mehua}"
+Olutvarasto: {olutta}")
+olutta.ota_varastosta(1000.0)
+saatiin {saatiin}
+Olutvarasto: {olutta}''')
     saatiin = mehua.ota_varastosta(-32.9)
-    print(f"saatiin {saatiin}")
-    print(f"Mehuvarasto: {mehua}")
+    print(f'''Mehuvarasto: {mehua}
+mehua.otaVarastosta(-32.9)
+saatiin {saatiin}
+Mehuvarasto: {mehua}''')
 
 
 if __name__ == "__main__":
